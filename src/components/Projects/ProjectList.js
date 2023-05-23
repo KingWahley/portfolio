f18140a93e720d9e9ada14d51077c1e18f2544f1
kingwahley <kingwahley@gmail.com>
2023-05-23 07:23:43 +0100

    new commit

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -4,6 +4,9 @@ import contact from "./assets/contact.png";
 import geoGuide from "./assets/geoGuide.png";
 import tellStory from "./assets/tellStory.png";
 import quizzy from "./assets/quizzy.png";
+import blog from "./assets/blog.png";
+import proj2 from "./assets/proj2.JPG";
+import space from "./assets/space.png";
 
 export const ProjectList = [
   {
@@ -23,9 +26,23 @@ export const ProjectList = [
   {
     name: "TellStory",
     image: tellStory,
-    skills: "html,css,javaScript,bootstrap",
+    skills: "html,css,javaScript",
     url: "https://tellStory.netlify.app",
     des: "Whether you're in the mood for a thrilling mystery, a heartwarming romance, or a hilarious comedy, Simply select your preferred genre and hit the button.",
+  },
+  {
+    name: "The Olawale Blog",
+    image: blog,
+    skills: "html,css,javaScript",
+    url: "https://theolawaleblog.netlify.app",
+    des: "Discover timeless articles and stories that captivates the mind.",
+  },
+  {
+    name: "Quizzy !",
+    image: quizzy,
+    skills: "JavaScript,HTML,CSS",
+    url: "https://quizzyweb.netlify.app/",
+    des: "Test your knowledge on several areas ranging from general Knowledge to mathematics and several other areas while having fun with a really engaging user interface.",
   },
   {
     name: "Emoji Factory",
@@ -35,17 +52,24 @@ export const ProjectList = [
     des: "Create ur own unique emojis and share them with friends with our easy to use emoji maker, you can customize your own expressions and characters",
   },
   {
+    name: "Previllous Bakes",
+    image: proj2,
+    skills: "JavaScript,HTML,CSS",
+    url: "https://prevellousbakes.netlify.app/",
+    des: "A cart system for a bakery website.",
+  },
+  {
+    name: "Space Stories",
+    image: space,
+    skills: "JavaScript,HTML,CSS",
+    url: "https://generatespacestories.netlify.app/",
+    des: "fill up text generator, lorem ipsum is boring.",
+  },
+  {
     name: "Add Contacts",
     image: contact,
     skills: "React,JavaScript,HTML,CSS",
     url: "https://addcontactlist.netlify.app",
     des: "A simple tool for saving and adding contacts to a contact List",
-  },
-  {
-    name: "Quizzy !",
-    image: quizzy,
-    skills: "JavaScript,HTML,CSS",
-    url: "https://quizzyweb.netlify.app/",
-    des: "Test your knowledge on several areas ranging from general Knowledge to mathematics and several other areas while having fun with a really engaging user interface.",
   },
 ];
